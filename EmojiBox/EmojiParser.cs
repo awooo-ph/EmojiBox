2e17a0548a9129737656f9796c3974d649dc9cd4
JaykeBird <jacobrh94@hotmail.com>
2016-05-27 01:13:38 -0500

    Added emoji library, basic parser support
    Added the emoji library from EmojiOne. Removed the "512 x 512" images because they really increased the size of the output DLL. (It's still 5 MB, but whatever). Also added a EmojiBoxSample project with a basic window to test out the library on! All buttons in the window work. (We have a working emoji name parser, woo!)

--- EmojiBox/EmojiParser.cs
+++ EmojiBox/EmojiParser.cs
@@ -3,10 +3,140 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
 
 namespace EmojiBox
 {
     public class EmojiParser
     {
+
+        private Dictionary<string, Emoji> emojis = new Dictionary<string, Emoji>();
+
+        public EmojiParser()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string jsonFile = "EmojiBox.emoji.json";
+
+            using (Stream s = assembly.GetManifestResourceStream(jsonFile))
+            {
+                JsonTextReader jtr = new JsonTextReader(new StreamReader(s));
+                JsonSerializer js = new JsonSerializer();
+
+                while (jtr.Read())
+                {
+                    emojis = js.Deserialize<Dictionary<string, Emoji>>(jtr);
+                }
+            }
+        }
+
+        public List<string> ListEmojisWithUnicodes()
+        {
+            List<string> elist = new List<string>();
+
+            foreach (KeyValuePair<string, Emoji> item in emojis)
+            {
+                elist.Add(item.Key + " (" + item.Value.Unicode + ")");
+            }
+
+            return elist;
+        }
+
+        Emoji Find(string name, bool useShortNameToo = true)
+        {
+            Emoji em = null;
+            foreach (Emoji item in emojis.Values)
+            {
+                if (item.Name == name || (useShortNameToo && item.ShortName == (":" + name + ":")))
+                {
+                    em = item;
+                    return em;
+                }
+            }
+
+            if (em == null)
+            {
+                throw new KeyNotFoundException("There is not an emoji by the name \"" + name + "\".");
+            }
+
+            return em;
+        }
+
+        /// <summary>
+        /// Gets or sets whether large emoji images should be returned.
+        /// <para />
+        /// If <code>true</code>, emoji images will be 128 x 128 in size. If <code>false</code>, emoji images will be 64 x 64 in size. (Height by width)
+        /// </summary>
+        public bool UseLargeImages { get; set; } = false;
+
+        BitmapImage GetEmojiImage(Emoji em)
+        {
+            try
+            {
+                if (UseLargeImages)
+                {
+                    return new BitmapImage(new Uri("pack://application:,,,/EmojiBox;component/Emoji/128/" + em.Unicode + ".png", UriKind.RelativeOrAbsolute));
+                }
+                else
+                {
+                    return new BitmapImage(new Uri("pack://application:,,,/EmojiBox;component/Emoji/64/" + em.Unicode + ".png", UriKind.RelativeOrAbsolute));
+                }
+            }
+            catch (IOException e)
+            {
+                throw new ArgumentException("Cannot find an emoji with the name \"" + em.Name + "\" (Unicode symbol \"" + em.Unicode + "\").", nameof(em), e);
+            }
+        }
+
+        /// <summary>
+        /// Get a BitmapImage of an emoji with the specified name.
+        /// </summary>
+        /// <param name="name">The name of the emoji.</param>
+        /// <exception cref="ArgumentException">Thrown if there is not an emoji by the specified name.</exception>
+        /// <returns>A BitmapImage of the specified emoji. Unless <see cref="UseLargeImages"/> is set, the image will be 64 x 64 in size (height by width). </returns>
+        public BitmapImage GetEmojiWithName(string name)
+        {
+            try
+            {
+                return GetEmojiImage(Find(name, true));
+            }
+            catch (KeyNotFoundException e)
+            {
+                throw new ArgumentException("There is not an emoji by the name \"" + name + "\".", nameof(name), e);
+            }
+            catch (ArgumentException e)
+            {
+                throw new ArgumentException("There is not an emoji by the name \"" + name + "\".", nameof(name), e);
+            }
+        }
+
+        /// <summary>
+        /// Get a BitmapImage of an emoji with the specified Unicode symbol.
+        /// </summary>
+        /// <param name="unicode">The unicode symbol. Do not include any leading marks like "#" or "\u".</param>
+        /// <exception cref="ArgumentException">Thrown if there is not an emoji with the specified Unicode symbol.</exception>
+        /// <returns>A BitmapImage of the specified emoji. Unless <see cref="UseLargeImages"/> is set, the image will be 64 x 64 in size (height by width). </returns>
+        public BitmapImage GetEmojiWithUnicode(string unicode)
+        {
+            try
+            {
+                if (UseLargeImages)
+                {
+                    return new BitmapImage(new Uri("pack://application:,,,/Emoji/128/" + unicode + ".png"));
+                }
+                else
+                {
+                    return new BitmapImage(new Uri("pack://application:,,,/Emoji/64/" + unicode + ".png"));
+                }
+            }
+            catch (IOException e)
+            {
+                throw new ArgumentException("Cannot find an emoji with the Unicode symbol \"" + unicode + "\".", nameof(unicode), e);
+            }
+        }
+
     }
 }
---
+++ EmojiBoxSample/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using EmojiBox;
+
+namespace EmojiBoxSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ep = new EmojiParser();
+        }
+
+        EmojiParser ep;
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (string item in ep.ListEmojisWithUnicodes())
+            {
+                TextBlock tb = new TextBlock();
+                tb.Text = item;
+                tb.HorizontalAlignment = HorizontalAlignment.Left;
+                tb.VerticalAlignment = VerticalAlignment.Top;
+                tb.Tag = item.Substring(0, item.IndexOf(' '));
+                tb.Focusable = true;
+                tb.KeyUp += Tb_KeyUp;
+                tb.MouseUp += Tb_MouseUp;
+
+                emojiList.Children.Add(tb);
+            }
+        }
+
+        private void Tb_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                txtEmojiName.Text = (sender as TextBlock).Tag as string;
+            }
+            //throw new NotImplementedException();
+        }
+
+        private void Tb_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter || e.Key == System.Windows.Input.Key.Space)
+            {
+                txtEmojiName.Text = (sender as TextBlock).Tag as string;
+            }
+            //throw new NotImplementedException();
+        }
+
+        private void btnSearchName_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                emojiImage.Source = ep.GetEmojiWithName(txtEmojiName.Text);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There is no Emoji by the name \"" + txtEmojiName.Text + "\".");
+            }
+        }
+    }
+}
